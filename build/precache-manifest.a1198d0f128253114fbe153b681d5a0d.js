3626bd0fd84b7ea1c71879daa6fa79a3bd75122e
paytoshj2 <44882335+paytoshj2@users.noreply.github.com>
2020-03-08 16:19:41 -0400

    Stripe testing

---
+++ build/precache-manifest.a1198d0f128253114fbe153b681d5a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9184e2814df2c8b8c643fd2c8c30daae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81dfa66b50df366883be",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8acb71875345ba94445a",
+    "url": "/static/js/2.7860f063.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7860f063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81dfa66b50df366883be",
+    "url": "/static/js/main.583eb85a.chunk.js"
+  },
+  {
+    "revision": "c27f76a36f8d51c29fd4",
+    "url": "/static/js/runtime-main.7bc453c4.js"
+  }
+]);
