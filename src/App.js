6751d5847d0b87146b225f01e45b9a9498b1cb3d
paytoshj2 <44882335+paytoshj2@users.noreply.github.com>
2020-03-10 22:18:15 -0400

    reverting to original format for neater merging

--- src/App.js
+++ src/App.js
@@ -1,77 +1,26 @@
-import React from 'react'
-import StripeCheckout from 'react-stripe-checkout';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default class TakeMoney extends React.Component {
-  onToken = (token) => {
-    fetch('/save-stripe-token', {
-      method: 'POST',
-      body: JSON.stringify(token),
-    }).then(response => {
-      response.json().then(data => {
-        alert(`We are in business, ${data.email}`);
-      });
-    });
-  }
-
-  // ...
-
-
-  render() {
-    return (
-      // ...
-      <StripeCheckout
-        token={this.onToken}
-        stripeKey="pk_test_rpJFYMoN3dlgpDND53RFbjz800n6Rl2nMN"
-        name = "Auctionly"
-        amount = {1000000000000}
-      />
-    )
-  }
-
-
-
-
-
-  // List of props that can be changed within this checkout button
-
-  /* <StripeCheckout
-  name="Three Comma Co." // the pop-in header title
-  description="Big Data Stuff" // the pop-in header subtitle
-  image="https://stripe.com/img/documentation/checkout/marketplace.png" // the pop-in header image (default none)
-  ComponentClass="div"
-  label="Buy the Thing" // text inside the Stripe button
-  panelLabel="Give Money" // prepended to the amount in the bottom pay button
-  amount={1000000} // cents
-  currency="USD"
-  stripeKey="..."
-  locale="zh"
-  email="info@vidhub.co"
-  // Note: Enabling either address option will give the user the ability to
-  // fill out both. Addresses are sent as a second parameter in the token callback.
-  shippingAddress
-  billingAddress={false}
-  // Note: enabling both zipCode checks and billing or shipping address will
-  // cause zipCheck to be pulled from billing address (set to shipping if none provided).
-  zipCode={false}
-  alipay // accept Alipay (default false)
-  bitcoin // accept Bitcoins (default false)
-  allowRememberMe // "Remember Me" option (default true)
-  token={this.onToken} // submit callback
-  opened={this.onOpened} // called when the checkout popin is opened (no IE6/7)
-  closed={this.onClosed} // called when the checkout popin is closed (no IE6/7)
-  // Note: `reconfigureOnUpdate` should be set to true IFF, for some reason
-  // you are using multiple stripe keys
-  reconfigureOnUpdate={false}
-  // Note: you can change the event to `onTouchTap`, `onClick`, `onTouchStart`
-  // useful if you're using React-Tap-Event-Plugin
-  triggerEvent="onTouchTap"
-  >
-  <button className="btn btn-primary">
-    Use your own child component, which gets wrapped in whatever
-    component you pass into as "ComponentClass" (defaults to span)
-  </button>
-</StripeCheckout> */
-
-
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
