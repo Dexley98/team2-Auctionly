b39920393c792c4822b504c6d4bc979b38d6e8eb
alhazmif2 <49456632+alhazmif2@users.noreply.github.com>
2020-03-12 23:13:58 -0400

    Add files via upload

---
+++ components/Navigation/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import SignOutButton from '../SignOut';
+import * as ROUTES from '../../constants/routes';
+
+const Navigation = () => (
+    <div>
+        <ul>
+            <li>
+                <Link to={ROUTES.SIGN_IN}>Sign in</ Link>
+            </li>
+            <li>
+                <Link to={ROUTES.SIGN_UP}>Sign up</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ADMIN}>Admin</Link>
+            </li>
+            <li>
+                <SignOutButton />
+            </li>
+        </ul>
+    </div>
+);
+
+export default Navigation;
