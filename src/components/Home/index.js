e790062b26d75e495d8c3f1ec155cbe9090aba81
paytoshj2 <44882335+paytoshj2@users.noreply.github.com>
2020-03-29 18:15:02 -0400

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { compose } from 'recompose';
-
+import Item from '../Item/item.js'
 import { WithAuthorization, WithEmailVerification } from '../Session';
 
 const HomePage = () => 
 
+        <React.Fragment>
             <div>
                 <h1>Home Page</h1>
                 <p>The Home Page is accessible by every signed in user.</p>
+
+                
+                {/* Change so that each item is generated from realtime database */}
+                <Item
+                    itemName="Dog"
+                    itemAmount="500">
+                </Item>
+
+
             </div>
+        </React.Fragment>
 
 const condition = authUser => !!authUser;
 
+// export default HomePage
 export default compose(
     WithEmailVerification,
     WithAuthorization(condition),
