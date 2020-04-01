f89607f17b3268e68d72fa5889cb3ee1b6da6b10
paytoshj2 <44882335+paytoshj2@users.noreply.github.com>
2020-03-31 23:23:09 -0400

    Adding bidding functionality

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,99 +1,171 @@
 import React, { Component } from 'react';
-
-//import routing from react router dom
-import {Link, BrowserRouter as Router, Route } from 'react-router-dom'
-
 import { compose } from 'recompose';
-import Firebase from 'firebase';
-
-import { withFirebase } from '../Firebase';
 import { WithAuthorization, WithEmailVerification } from '../Session';
-import * as ROLES from '../../constants/roles';
 
 
-import ItemPage from '../Item';
 
-class HomePage extends Component{
+class ItemPage extends Component{
 
     constructor(props) {
         super(props);
-        this.state = {
+       this.state = {
             loading: false,
-            items: [],
+            item: {},
+            key: "",
+            bidValue: '',
+            highestBid: 0,
+            highestBidder: ""
         };
+        
     }
 
-componentDidMount() {
-    this.setState({ loading: true });
+    componentDidMount(){
+        this.setState({ loading: true });
+        let dbItemKey = urlStringToItemName(this.props.match.params[0]);
 
-    this.props.firebase.items().on('value', snapshot => {
-        const itemsObject = snapshot.val();
-        console.log(itemsObject);
-        const itemsList = Object.keys(itemsObject).map(key => ({
-            ...itemsObject[key],
-            id: key,
-        }));
-        this.setState({
-            items: itemsList,
-            loading: false,
-        });
-    });
-}
+        /***************************************************************************************************
+         * There is quite a lot going on in here so I'll try and explain it the best I can. 
+         * I Only called this once
+         * Justification: You are supposed to call .once if you want expect this UI element to remain the same.
+         * Once a bidder goes to this page, they should not expect it to change. 
+         * Willing to change this as necessary / as the team sees fit. 
+         * I am leaving all the console logs in here so everyone can see my pain.
+         * 
+         * Function Explanation
+         * So I added an index rule in the firebase database on item names. 
+         * We will probably have to change this once we get item upload functionality sorted out. 
+         * Basically I make a ref to the items collection, then I order it by name. 
+         * This makes sure that I get only the item in question. (dbItemName)
+         * I only get this item once, I explain why above. 
+         * 
+         * This next part is hell. I get a snapshot of the returned item in the itemObject, fairly routine stuff.
+         * Object.keys returns an array of the enumerable object. I wish there was another way to do this since there is only one key.
+         * but whatever. 
+         * 
+         * I have to take that key's first (and only) index and use it as the index on the itemObject to just get the item fields
+         * after that I just assign it to my state and move along my day.
+         * 
+         * This is rough, but I think it will scale. Sorry for the long comment. I just didn't know how this worked before. 
+         * I sure do now. 
+         * 
+         * 
+        *******************************************************************************************************/ 
+        this.props.firebase.db.ref("items")
+            .orderByChild("name")
+            .equalTo(dbItemKey)
+            .on('value', snapshot =>{
+                const itemObject = snapshot.val();
+                console.log('item object ', itemObject["three"]);
+                const keyList = Object.keys(itemObject);
+                console.log('key ', keyList)
+                console.log('item to assign to state ', itemObject[keyList[0]])
+                this.setState({
+                    item: itemObject[keyList[0]],
+                    key: keyList[0],
+                    loading: false
+                });
+            });
+        console.log('state.item ',this.state.item);
+    }
 
-render(){
-    const {items, loading} = this.state;
-    return(
-        <React.Fragment>
-        <div>
-            <h1>Home Page</h1>
-            <p>The Home Page is accessible by every signed in user.</p>
-        </div>
-        <ItemList items = {items}></ItemList>
-        </React.Fragment>
-    )
-}
-}
+    
+    render(){
+        const item = this.state.item;
+        const bidList = this.state.item['bidList']
+        let x = 0
+        for(x in bidList){
+            if(bidList[x] > this.state.highestBid)
+            {
+                this.setState({highestBid:bidList[x]})
+                // highestBid = bidList[x]
+                this.setState({highestBidder : x}) 
 
-// this function takes an item name from the item component and creates a url formatted like firstword-secondword-lastword
-const itemNametoUrlString = (itemName) => {
-    let splitNameList = itemName.split(' ');
-    let itemNameUrl = '';
+            }
+        }
+        // this.setState({highestBid:highestBidd})
+        return(
+            // same thing as React.Fragment / different syntax. May not need but here for now.
+            <>
+            <div>
+                <img src={item.imageUrl} width="200px" height = "200px"/>
+                <h1>{item.name}</h1>
+                <p>{item.description}</p>
+                <p>Start Price: ${(item.startPrice/1).toFixed(2)}</p>
+                <p> Highest Bid: ${(this.state.highestBid/1).toFixed(2)}</p>
+                <p>Buy it Now Price: ${(item.buyItNow/1).toFixed(2)}</p>
 
-    for(let i=0; i<splitNameList.length; i++){
-        if( i == splitNameList.length - 1){
-            itemNameUrl += splitNameList[i];
+                <form onSubmit={this.handleBid}>
+                $<input type="number" step="5" min={this.state.highestBid + 5} id="bidInput" onChange={this.myChangeHandler}></input><input type="submit" value="Bid"/> 
+                </form>
+
+                <form onSubmit={this.handleBuyout}>
+                <input type ="submit" value="Buyout"/>
+                </form>
+            </div>
+            </>
+        )
+    }
+
+    myChangeHandler = (event) => {
+        this.setState({bidValue:event.target.value})
+        
+    }
+
+    handleBid = (event) => {
+        event.preventDefault();
+        let bid = Number(this.state.bidValue)
+        if(this.state.item["available"] == true)
+        {
+            this.props.firebase.db.ref("items/"+ this.state.key + "/bidList").update({[this.props.firebase.auth.W]:bid})
         }
         else{
-            itemNameUrl += `${splitNameList[i]}-`;
+            alert("This item is no longer available!")
         }
     }
-    return itemNameUrl
+
+    handleBuyout = (event) => {
+        event.preventDefault();
+        let buyout = Number(this.state.item["buyItNow"])
+        if(this.state.highestBid > buyout)
+        {
+            alert("The bid is higher than the buyout!")
+        }
+        else
+        {
+            if(this.state.item["available"] == true)
+            {
+                this.props.firebase.db.ref("items/"+ this.state.key + "/bidList").update({[this.props.firebase.auth.W]:buyout})
+                this.props.firebase.db.ref("items/"+ this.state.key).update({available:false})
+            }
+            else{
+                alert("This item is no longer available!")
+            }
+        }
+    }
 }
 
 
-const ItemList = ({ items }) => (
-    <ul>
-        {items.map(item => (
-                <Link to={`item/${itemNametoUrlString(item.name)}`}>
-                    <li key={item.name}>
-                        <span>
-                            <img src={item.imageUrl} width="200px" height = "200px"/>
-                            <br />
-                            {item.name}
-                            <strong> Buy Now: </strong> {item.buyItNow}
-                            <strong> Starting Price: </strong> {item.startPrice}
-                            <br></br> {item.description}
-                    
-                        </span>
-                    </li>
-                </Link>
-        ))}
-    </ul>
-);
+const urlStringToItemName = (param) => {
+    //split url on - delim
+    let splitUrlList = param.split('-');
+    let itemNameString = '';
+
+    // create name string to ref to db. if index is last in array don't add space.
+    // if there are dashes in the name this WILL FUCK UP. Probably going to have to account for that in item upload.
+    for(let i=0; i<splitUrlList.length; i++){
+        if(i != splitUrlList.length - 1){
+            itemNameString += `${splitUrlList[i]} `;
+        }else{
+            itemNameString += splitUrlList[i];
+        }
+    }
+    // return string to name ref in db.
+    return itemNameString;
+};
 
 const condition = authUser => !!authUser;
 
 export default compose(
     WithEmailVerification,
     WithAuthorization(condition),
-)(HomePage);
+)(ItemPage);
