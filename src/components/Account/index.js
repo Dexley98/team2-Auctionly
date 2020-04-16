7dbe418b5ecd4fcb581b56ddf0674ebae82c110f
paytoshj2 <44882335+paytoshj2@users.noreply.github.com>
2020-04-16 08:05:08 -0400

    Add files via upload

---
+++ src/components/Account/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { compose } from 'recompose';
+import './index.css' ;
+
+// add Nav from 04/15/2020
+import Navigation from '../Navigation';
+
+import PasswordChangeForm from '../PasswordChange';
+import {
+    AuthUserContext,
+    WithAuthorization,
+    WithEmailVerification,
+} from '../Session';
+
+const AccountPage = () => (
+    <AuthUserContext.Consumer>
+        {authUser => (
+            <>
+                <Navigation />
+                {/* <hr /> */}
+                <div className="account-page-wrapper">
+                    <h1>Welcome {authUser.username}</h1>
+                    <h2>{authUser.email}</h2>
+                    <p>Reset password:</p>
+                    <PasswordChangeForm />
+                </div>
+            </>
+        )}
+    </AuthUserContext.Consumer>
+);
+
+const condition = authUser => !!authUser;
+
+export default compose(
+    WithEmailVerification,
+    WithAuthorization(condition),
+)(AccountPage);
