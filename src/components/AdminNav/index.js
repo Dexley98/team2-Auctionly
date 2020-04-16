353cec8f8bba169f1eb55c614a3e9c1fb4146432
paytoshj2 <44882335+paytoshj2@users.noreply.github.com>
2020-04-16 08:05:25 -0400

    Add files via upload

---
+++ src/components/AdminNav/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
+
+import { AuthUserContext } from '../Session';
+import SignOutButton from '../SignOut';
+import * as ROUTES from '../../constants/routes';
+
+const AdminNavigation = () => (
+    <AuthUserContext.Consumer>
+        {authUser =>
+            authUser ? (
+                <AdminNavigationAuth authUser={authUser} />
+            ) : (
+                    <AdminNavigationNonAuth />
+                )
+        }
+    </AuthUserContext.Consumer>
+);
+
+const AdminNavigationAuth = ({ authUser }) => (
+    <nav className="admin-nav-bar-wrapper">
+        <ul className="admin-nav-list-authorized">
+            <li>
+                <Link to={ROUTES.HOME}>Auction Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+        </ul>
+    </nav>
+);
+
+const AdminNavigationNonAuth = () => (
+    <nav className="nav-bar-wrapper">
+        <ul className="nav-list-non-authorized">
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default AdminNavigation;
