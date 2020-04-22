e85fadc3079be94d27dfb9bfe43f02f03dfa7057
paytoshj2 <44882335+paytoshj2@users.noreply.github.com>
2020-04-22 17:15:03 -0400

    adding 2 functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+var stripe = require('stripe')('sk_test_3ylFfZ8lM7Yv36r1FUXZlsoG00pWujDn1A');
+
+
+exports.checkout = functions.https.onCall( async(data, context) => {
+    const input = data.text.split("!")
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+          name: input[0],
+          description: input[2],
+          images: [input[3]],
+          amount: input[1] * 100,
+          currency: 'usd',
+          quantity: 1,
+        }],
+        success_url: 'https://auctionly-141e3.firebaseapp.com/home',
+        cancel_url: 'https://auctionly-141e3.firebaseapp.com/home',
+      });
+    
+      return session.id;
+
+});
+
+
+exports.helloWorld = functions.https.onCall((data, context) => {
+  return "hello world!";
+});
