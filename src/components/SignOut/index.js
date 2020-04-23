18457128857bd474dacbaa30e14e7e943ab35270
alhazmif2 <49456632+alhazmif2@users.noreply.github.com>
2020-04-22 21:10:50 -0400

    Update index.js

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,3 +1,6 @@
+/******************************************************************
+                    Sign out from the Auction
+ ******************************************************************/
 import React from 'react';
 import { withFirebase } from '../Firebase';
 
